22bb52a6acb624963231f60a9f0404734a13500d
amcereijo <amcereijo@gmail.com>
2013-12-26 20:51:50 +0100

    imports fixed

--- src/com/androlot/applicatioin/GameApplication.java
+++ src/com/androlot/applicatioin/GameApplication.java
@@ -1,6 +1,9 @@
 package com.androlot.applicatioin;
 
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
 import android.app.Application;
+import android.content.Context;
 
 import com.androlot.enums.GameTypeEnum;
 
@@ -13,5 +16,16 @@ public class GameApplication extends Application {
 	}
 	public static void setGameType(GameTypeEnum gameType) {
 		GameApplication.gameType = gameType;
+	}
+	
+	
+	public static boolean checkServiceRunning(Context context,  Class classType){
+		ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+	    for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+	        if (classType.getName().equals(service.service.getClassName())) {
+	            return true;
+	        }
+	    }
+	    return false;
 	}
 }
