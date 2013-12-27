9dbea9c98710c3373aab987ed6c10edfdffac8e4
amcereijo <amcereijo@gmail.com>
2013-12-27 01:40:26 +0100

    use and save of external context

--- src/com/androlot/application/GameApplication.java
+++ src/com/androlot/application/GameApplication.java
@@ -3,6 +3,7 @@ package com.androlot.application;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
 import android.app.Application;
+import android.content.ContentResolver;
 import android.content.Context;
 
 import com.androlot.enums.GameTypeEnum;
@@ -11,6 +12,7 @@ public class GameApplication extends Application {
 
 	private static GameTypeEnum gameType;
 	private static GameApplication gameApplication;
+	private static Context context;
 	
 	public static GameTypeEnum getGameType() {
 		return gameType;
@@ -20,7 +22,7 @@ public class GameApplication extends Application {
 	}
 	
 	public static boolean isServiceRunning(Class<?> classType){
-		ActivityManager manager = (ActivityManager) GameApplication.getInstance().getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
+		ActivityManager manager = (ActivityManager) GameApplication.getContext().getSystemService(Context.ACTIVITY_SERVICE);
 	    for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
 	        if (classType.getName().equals(service.service.getClassName())) {
 	            return true;
@@ -34,5 +36,12 @@ public class GameApplication extends Application {
 			gameApplication = new GameApplication();
 		}
 		return gameApplication;
+	}
+	
+	public static void setContext(Context context) {
+		GameApplication.context = context;
+	}
+	public static Context getContext() {
+		return context;
 	}
 }
