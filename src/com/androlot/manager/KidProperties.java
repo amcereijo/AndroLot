e7d6ea388278f73fe7b818dec131caf7c38c1321
amcereijo <amcereijo@gmail.com>
2013-12-27 17:28:35 +0100

    Add properties kid game load

--- src/com/androlot/manager/KidProperties.java
+++ src/com/androlot/manager/KidProperties.java
@@ -1,10 +1,34 @@
 package com.androlot.manager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+
+import com.androlot.application.GameApplication;
+
 public class KidProperties extends AbstractGamePropertiesLoadar {
 
 	private final static String FILE_NAME = "kid.conf.properties";
 	
 	public void doLoad() {
-		//TODO
+		Resources resources = GameApplication.getContext().getResources();
+		AssetManager assetManager = resources.getAssets();
+
+		try {
+			InputStream inputStream = assetManager.open(FILE_NAME);
+			Properties properties = new Properties();
+		    properties.load(inputStream);
+		    
+		    setDay(properties.getProperty("day"));
+			setMonth(properties.getProperty("month"));
+			setHour(properties.getProperty("hour"));
+			setMin(properties.getProperty("min"));
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
